0a2b333d5b093ffc94c74f46a6c1db9bc769082f
jmg2107 <jenjengoo@gmail.com>
2016-10-25 18:37:26 -0700

    (fix) Fix reference to deployed port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var server = require('./server/server.js');
 
-var port = process.env.PORT | 3000;
+var port = process.env.PORT || 3000;
 
 server.listen(port);
 console.log('Listening on port ' + port);
